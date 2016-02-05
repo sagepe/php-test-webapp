e01af609fc8b83fee0a26c2f28f65a30e45f75cd
Sam Pearson <sam.pearson@linuxit.com>
2016-02-05 09:19:08 +0000

    Fixed Typo. There is a purpose to this, it helps demonstrate stuff. Honest.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <title>Test Webapp - PHP</title>
   </head>
   <body>
-    <?php echo '<p>Hello World! Welone to our Generic Website.</p>'; ?>
+    <?php echo '<p>Hello World! Welcome to our Generic Website.</p>'; ?>
   </body>
 </html>
