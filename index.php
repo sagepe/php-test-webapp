d8697a3f6bc2994b05704fab05b2fcb694049fee
Sam Pearson <sam.pearson@linuxit.com>
2016-02-05 09:21:16 +0000

    Personalised for Science Warehouse

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <title>Test Webapp - PHP</title>
   </head>
   <body>
-    <?php echo '<p>Hello World! Welcome to our Generic Website.</p>'; ?>
+    <?php echo '<p>Hello World! This has been personalised for Science Warehouse"</p>'; ?>
   </body>
 </html>
